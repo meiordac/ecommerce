0a7dd49840ce814eefc73b93eb6cea28861613b1
meiordac <matias.iordache@gmail.com>
2016-02-13 20:10:16 -0300

    modified catalog admin and database

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category
+from .models import Product
+
+admin.site.register(Category)
+admin.site.register(Product)
